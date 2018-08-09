12c1e5210b09d6907a8aade844a60edeedf7158a
Dani Schuhman <dani8439@aol.com>
2018-08-09 15:10:01 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,10 +31,9 @@ def can_be_created_in_a_block(args = *x)
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = 
-    m.release_date 
+    m.title =
+    m.release_date =
     m.save
-    
   end
 end
 
